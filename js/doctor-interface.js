5171e086bef72365b8b02e9aece429f110fc82f8
Brian Rush <yankeh@gmail.com>
2017-06-30 10:09:06 -0700

    set up initial folder and file structure and add basic scaffolding, including my apiKey variable, package.json, bower.json, link and scripts in html head, and gulp tasks in gulpfile.js (have not built or served yet)

---
+++ js/doctor-interface.js
@@ -0,0 +1,10 @@
+// var ExampleModule = require('./../js/scripts.js').exampleModule;
+var apiKey = require('./../.env').apiKey;
+
+
+
+$(document).ready(function() {
+  // var exampleInstance = new ExampleModule('args');
+
+  // console.log(exampleInstance.examplePrototype());
+});
