59d21b8a57d498e4108246371c6111cb98a19962
Brian Rush <yankeh@gmail.com>
2017-06-30 17:05:58 -0700

    query the API and console log appropriate doctors based on user input

--- js/doctor.js
+++ js/doctor.js
@@ -1,10 +1,36 @@
-//business logic stuff for a particular module
-var ExampleModule = function(args) {
-  this.args = args; //to be replaced with constructor arguments
+//business logic for retrieving doctor info
+var apiKey = require('./../.env').apiKey;
+console.log("the key is " + apiKey);
+
+function DoctorsObject(firstName, lastName, location, specialties, photo, url) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.fullName = firstName + lastName;
+  this.location = location;
+  this.specialties = specialties;
+  this.photo = photo;
+  this.webSite = url;
+}
+
+DoctorsObject.prototype.getDoctors = function(medicalIssue) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+   .then(function(result) {
+      console.log(result);
+    })
+   .fail(function(error){
+      console.log("fail");
+    });
 };
 
-ExampleModule.prototype.examplePrototype = function() {
-  return `this is an example prototype method`;
-};
+exports.doctorModule = DoctorsObject;
 
-exports.exampleModule = ExampleModule;
+
+// var ExampleModule = function(args) {
+//   this.args = args; //to be replaced with constructor arguments
+// };
+//
+// ExampleModule.prototype.examplePrototype = function() {
+//   return `this is an example prototype method`;
+// };
+//
+// exports.exampleModule = ExampleModule;
